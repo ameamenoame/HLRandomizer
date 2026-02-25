a703d1a47c23c0090c1b78c5cad681f073469ef4
amenoamea <falling_snowdin@protonmail.com>
2026-02-25 12:34:02 +0700

    Remove south dash challenge from random start pool

--- preset.py
+++ preset.py
@@ -100,7 +100,7 @@ class Preset:
                 or HLDBasics.room_names[c][0].startswith("rm_pax") \
                 or HLDBasics.room_names[c][0] in ["rm_carena", "rm_televatorshaft"] \
                 or c >= 220 \
-                or c in [133, 134, 135, 183, 232, 123, 250]:
+                or c in [132, 133, 134, 135, 183, 232, 123, 250]:
                 continue
             print("Naked start: " + HLDBasics.room_names[c][0] + " - " + HLDBasics.room_names[c][1])
             room_choice = c 
